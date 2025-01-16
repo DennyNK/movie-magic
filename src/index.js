293c0182b7f59c8870871fc047e41f9a83e88042
DennyNK <dnkovacheva@gmail.com>
2025-01-16 10:27:26 +0200

    Install and set up express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('1312')
+import express from 'express';
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.listen(1000, () => console.log('Server is listening on http://localhost:1000...'));
