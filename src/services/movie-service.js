4a02e98232b4ecedadae5d8d744d4da2bad8143b
DennyNK <dnkovacheva@gmail.com>
2025-01-18 12:36:44 +0200

    add create functionality

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,10 +1,18 @@
 import movies from "../movies.js";
-
+import { v4 as uuid } from 'uuid';
 export default {
     findMovie(movieId){
         const resultMovie = movies.find(movie => movie.id === movieId);
     
         return resultMovie;
     },
+    create(movieData) {
+        const newId = uuid();
+        movies.push({
+            id: newId,
+            ...movieData,
+        });
+        return newId;
+    }
 
 }
