44a0354c4d1ff6c209d8c43dd4245ce2174c00cc
DennyNK <dnkovacheva@gmail.com>
2025-01-23 15:38:47 +0200

    set allow properties to add custom ones

--- src/index.js
+++ src/index.js
@@ -19,6 +19,9 @@ try{
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+    },
     helpers: {showRating}
 }));
 app.set('view engine', 'hbs');
