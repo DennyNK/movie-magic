cb19d6293d3a99b5e336dcb0dd96f556e1d8b24f
DennyNK <dnkovacheva@gmail.com>
2025-01-24 13:16:35 +0200

    add cast router

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import homeController from './controllers/homeController.js';
 import { Router } from 'express';
 import movieController from './controllers/movieController.js';
+import castController from './controllers/castController.js';
 
 const routes = Router();
 
 routes.use(homeController);
 routes.use('/movies', movieController);
+routes.use('/casts', castController);
 
 routes.get('*', (req, res) => {
     res.render('404');
