a70e065b0006030ab32d78ae14fa5b3f5c1442f9
DennyNK <dnkovacheva@gmail.com>
2025-02-01 12:26:10 +0200

    login page and functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,8 +15,22 @@ authController.post('/register', async (req, res) => {
     res.redirect('/auth/login');
 });
 
-authController.get('/login', async (req, res) => {
+authController.get('/login', (req, res) => {
     res.render('auth/login');
+});
+
+authController.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        await authService.login(email, password);
+    } catch(err){
+        console.log(err.message);
+        return res.redirect(404);
+    }
+
+    res.redirect('/')
+
 });
 
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,10 +1,26 @@
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
 
 export default {
 
-    register(userData){
+    register(userData) {
         return User.create(userData);
-        
+
+    },
+
+    async login(email, password) {
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error('Invalid login');
+        };
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            throw new Error('Invalid login');
+        }
+
+        return;
     }
 
 }
