8c7f25522c33be13e25e0d967d1b0352a6dd8668
DennyNK <dnkovacheva@gmail.com>
2025-02-06 15:40:43 +0200

    add validation properties for cast and movie

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -2,11 +2,26 @@ import { Schema, model } from "mongoose";
 
 const castSchema = new Schema({
 
-    name: String,
-    age: Number,
-    born: String,
-    // nameInMovie: String,
-    image: String,
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        minlength: [5, 'Name should be at least 5 character slong'],
+        match: [/^[a-zA-Z 0-9]+$/, 'Name should be alphanumeric, digits and whitespaces']
+    },
+    age: {
+        type: Number,
+        min: 0,
+        max: 120
+    },
+    born: {
+        type: String,
+        minlength: [10, 'Place of birth should be at least 10 character slong'],
+        match: [/^[a-zA-Z 0-9]+$/, 'Place of birth should be alphanumeric, digits and whitespaces'] 
+    },
+    image: {
+        type: String,
+        match: [/^https?\:\/\//, 'Image url should start with http:// or https://']
+    },
     // movie: '',
 
 });
--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,14 +1,45 @@
 import { Schema, model, Types } from "mongoose";
 
 const movieSchema = new Schema({
-title: String,
+title: {
+    type: String,
+    required: [true, 'Title is required'],
+    minlength: [5, 'Title should be at least 5 character slong'],
+    match: [/^[a-zA-Z 0-9]+$/, 'Title should be alphanumeric, digits and whitespaces']
+},
 category: String,
-genre: String,
-director: String,
-year: Number,
-image: String,
-rating: Number,
-description: String,
+genre: {
+    type: String,
+    required: [true, 'Genre is required'],
+    minlength: [5, 'Genre should be at least 5 character slong'],
+    match: [/^[a-zA-Z 0-9]+$/, 'Genre should be alphanumeric, digits and whitespaces']
+},
+director: {
+    type: String,
+    // required: [true, 'Director is required'],
+    minlength: [5, 'Director should be at least 5 character slong'],
+    match: [/^[a-zA-Z 0-9]+$/, 'Director should be alphanumeric, digits and whitespaces']
+},
+year: {
+    type: Number,
+    min: 1900,
+    max: 2024
+},
+image: {
+    type: String,
+    match: [/^https?\:\/\//, 'Image url should start with http:// or https://']
+},
+rating: {
+    type: Number,
+    min: 1,
+    max: 5
+},
+description: {
+    type: String,
+    minlength: [20, 'Description should be at least 20 character slong'],
+    match: [/^[a-zA-Z 0-9]+$/, 'Description should be alphanumeric, digits and whitespaces']
+
+},
 cast: [{
     type: Types.ObjectId,
     ref: 'Cast'
